ce60e042a3dd25f47f22287853b5ac56f0937c28
albatool <a23@test.com>
2022-05-26 15:32:17 +0300

    first commit

---
+++ loop.js
@@ -0,0 +1,52 @@
+/* 1Print odds*/
+
+// for(var i=1; i<= 20; i++){
+//     console.log(i)
+// }
+
+
+/* 2 Decreasing Multiples of 3*/
+
+// for(var i=0; i<= 100;i++ ){
+//     if (i % 3 == 0){
+
+    
+//     console.log(i)}
+// }
+
+
+
+/* 3 Print the sequence*/
+
+    var arr= []
+for( var i = 4; i  > -4 ;i -=1.5 ){
+    arr.push(i);
+    
+    
+}
+
+console.log(arr)
+
+
+
+
+
+/* 4 Sigma*/
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+
+
+
+/* 5 Factorial*/
+
+//  let sum = 1;
+// for (let i = 1; i <= 12; i++) {
+//     sum = sum * i;
+// }
+// console.log(sum)
+
